2a2baf570b8f651172806e7d57cd73c742659360
Chitraansh <chitraansh.anand.univ@gmail.com>
2026-06-30 13:11:15 +0530

    Add main menu UI and scene navigation

---
+++ unity/StorytellingVR/Assets/_Project/Scripts/MainMenu/MenuSceneButton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneButton : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+
+    public void LoadScene()
+    {
+        if (string.IsNullOrWhiteSpace(sceneName))
+        {
+            Debug.LogError($"{nameof(MenuSceneButton)} on '{gameObject.name}' cannot load a scene because sceneName is empty.");
+            return;
+        }
+
+        Debug.Log($"{nameof(MenuSceneButton)} on '{gameObject.name}' loading scene '{sceneName}'.");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log($"{nameof(MenuSceneButton)} on '{gameObject.name}' quitting the application.");
+        Application.Quit();
+    }
+}
